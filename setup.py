cb45856b4d8acfdfc358128b0962ee7a67cbffe4
jmunozmendiFunditec <jmunoz@funditec.es>
2024-02-28 09:18:13 +0100

    Fix singular covariance

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SNGP",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     description="Spectral-normalized Neural Gaussian processes in PyTorch.",
     long_description=open('README.md').read(),
